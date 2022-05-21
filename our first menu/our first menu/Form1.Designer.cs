e481fc497d3894bb97b824e681027cfa92854610
eRobda <51453874+eRobda@users.noreply.github.com>
2022-05-21 22:09:01 +0200

    Add files via upload

---
+++ our first menu/our first menu/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace our_first_menu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_main = new System.Windows.Forms.Panel();
+            this.panel_name = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.panel_main.SuspendLayout();
+            this.panel_name.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel_main
+            // 
+            this.panel_main.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.panel_main.Controls.Add(this.groupBox2);
+            this.panel_main.Controls.Add(this.groupBox1);
+            this.panel_main.Controls.Add(this.panel_name);
+            this.panel_main.Location = new System.Drawing.Point(12, 12);
+            this.panel_main.Name = "panel_main";
+            this.panel_main.Size = new System.Drawing.Size(382, 236);
+            this.panel_main.TabIndex = 0;
+            // 
+            // panel_name
+            // 
+            this.panel_name.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.panel_name.Controls.Add(this.label1);
+            this.panel_name.Controls.Add(this.label2);
+            this.panel_name.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_name.ForeColor = System.Drawing.Color.Black;
+            this.panel_name.Location = new System.Drawing.Point(0, 0);
+            this.panel_name.Name = "panel_name";
+            this.panel_name.Size = new System.Drawing.Size(382, 66);
+            this.panel_name.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comfortaa", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(132)))), ((int)(((byte)(252)))));
+            this.label1.Location = new System.Drawing.Point(158, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comfortaa", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.label2.Location = new System.Drawing.Point(-1, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(699, 60);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust \r\nRust Rust" +
+    " Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust Rust ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Comfortaa", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(132)))), ((int)(((byte)(252)))));
+            this.groupBox1.Location = new System.Drawing.Point(4, 72);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(115, 157);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Aimbot";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Comfortaa", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkBox1.Location = new System.Drawing.Point(15, 21);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(78, 23);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Enabled";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Comfortaa", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkBox2.Location = new System.Drawing.Point(15, 40);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(46, 23);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Idk";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox3);
+            this.groupBox2.Controls.Add(this.checkBox4);
+            this.groupBox2.Font = new System.Drawing.Font("Comfortaa", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.groupBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(132)))), ((int)(((byte)(252)))));
+            this.groupBox2.Location = new System.Drawing.Point(125, 72);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(248, 157);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "ESP";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Comfortaa", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkBox3.Location = new System.Drawing.Point(15, 40);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(46, 23);
+            this.checkBox3.TabIndex = 1;
+            this.checkBox3.Text = "Idk";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("Comfortaa", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkBox4.Location = new System.Drawing.Point(15, 21);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(78, 23);
+            this.checkBox4.TabIndex = 0;
+            this.checkBox4.Text = "Enabled";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 255);
+            this.Controls.Add(this.panel_main);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.panel_main.ResumeLayout(false);
+            this.panel_name.ResumeLayout(false);
+            this.panel_name.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_main;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Panel panel_name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ our first menu/our first menu/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace our_first_menu
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(Keys vKey);
+
+        static bool showing = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //make the windows transparent
+            this.BackColor = Color.Wheat;
+            this.TransparencyKey = Color.Wheat;
+
+            //make the windows borderless
+            this.FormBorderStyle = FormBorderStyle.None;
+
+            //make the windows start in top left corner
+            this.StartPosition = FormStartPosition.Manual;
+
+            //make the windows topmost
+            this.TopMost = true;
+
+            CheckForIllegalCrossThreadCalls = false;
+
+            Thread shm = new Thread(ShowHideMenu);
+            shm.Start();
+        }
+
+        void ShowHideMenu()
+        {
+            while (true)
+            {
+                if(GetAsyncKeyState(Keys.Insert) < 0 && showing == true) //than hide it
+                {
+                    this.Hide();
+                    showing = false;
+                    Thread.Sleep(20);
+                }
+                else if (GetAsyncKeyState(Keys.Insert) < 0 && showing == false) // than show it
+                {
+                    this.Show();
+                    showing = true;
+                    Thread.Sleep(20);
+                }
+                else if (GetAsyncKeyState(Keys.Delete) < 0)
+                {
+                    Environment.Exit(0);
+                    Application.Exit();
+                }
+
+                Thread.Sleep(70);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //end all created threads
+            Environment.Exit(0);
+            Application.Exit();
+        }
+    }
+}
